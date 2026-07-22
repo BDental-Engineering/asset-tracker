1fe274d891f13e5bd5c2edb595296f430c47ea0c
BDental-Engineering <info@bdentalengineering.co.uk>
2026-07-22 08:00:20 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -18,7 +18,7 @@ module.exports = function(req, res) {
     response_type: 'code',
     client_id:     process.env.SM8_CLIENT_ID,
     redirect_uri:  process.env.SM8_REDIRECT_URI,
-    scope:         'manage_assets read_customers manage_customers read_customer_contacts manage_staff read_staff read_job_contacts manage_job_contacts read_jobs manage_jobs create_jobs read_job_materials read_job_categories read_job_queues read_job_payments read_tasks read_schedule staff_activity staff_locations read_job_checklists',
+    scope:         'manage_assets read_customers manage_customers read_customer_contacts manage_staff read_staff read_job_contacts manage_job_contacts read_jobs manage_jobs create_jobs read_job_materials read_job_categories read_job_queues read_job_payments read_tasks read_schedule staff_activity staff_locations read_job_checklists manage_job_checklists',
     state:         statePayload
   });
 
